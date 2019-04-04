20a9efb456ee380a013b29e332cba609f8af7f3b
Narutoskee <narutoskee@gmail.com>
2019-04-04 16:13:29 +0300

    add first task - start data

---
+++ first.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: snabweb
+ * Date: 004 04.04.19
+ * Time: 15:59
+ */
+//make analog of var_dump
+function debug($var)
+{
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+}
+
+$m = [
+    [4, 1, 7],
+    [8, 3, 2],
+    [2, 9, 5]
+];
